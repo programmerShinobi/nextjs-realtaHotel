6563cda87660f7d01424e24a646c32465bc7d66b
FaQih <110642627+programmerShinobi@users.noreply.github.com>
2023-02-04 21:06:47 +0700

    Update LayoutSignIn.js

--- components/LayoutSignIn.js
+++ components/LayoutSignIn.js
@@ -4,19 +4,28 @@ import { Card } from '@mui/material';
 export default function LayoutSignIn({ children }) {
   return (
     <>
-      <div className=" bgLayout">
-        <div className={styles.logoLayout}></div>
-        <Card responsi className={styles.cardLogin}>
-          <div className="form-auth">
-            <div className="py-8">
-              {children}
+      <div className="bgLayout">
+        <div class="mx-auto sm:mx-0">
+          <div className={styles.logoLayout}></div>
+          <Card responsi className={styles.cardLogin}>
+            <div className="form-auth">
+              <div className="py-8">
+                {children}
+              </div>
             </div>
-          </div>
-        </Card>
-        <div className={styles.bgImg}>
+          </Card>
+        </div>
+        <div class="hidden md:block">
+          <div className={styles.bgImg} />
           <div className={styles.cardInCardLogin}>
-            <div className={styles.textTitlaInCardLogin}>
-              <b>Give More <u>Experience</u><br />With Our Service</b>
+            <div className={styles.textTitlaInCardLogin1}>
+              <b>Give More</b>
+            </div>
+            <div className={styles.textTitlaInCardLogin2}>
+              <b><u>Experience</u></b>
+            </div>
+            <div className={styles.textTitlaInCardLogin3}>
+              <b>With Our Service</b>
             </div>
             <div className={styles.textSubtitleInCardLogin}>
               Hear what our guests have to say about their unforgettable stay at our hotel
