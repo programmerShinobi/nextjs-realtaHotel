401b3f404f0d8a95ed131d9d26df4fc3a20c0f53
FaQih <110642627+programmerShinobi@users.noreply.github.com>
2023-02-06 08:10:56 +0700

    Update TobBar.js

--- components/TobBar.js
+++ components/TobBar.js
@@ -138,8 +138,8 @@ export default function TopBar({ showNav, setShowNav }) {
             leaveFrom="transform scale-100"
             leaveTo="transform scale-95"
           >
-            <Menu.Items className="absolute right-0 w-56 z-50 mt-2 origin-top-right bg-white rounded shadow-sm">
-              <div className="p-2">
+            <Menu.Items className="absolute right-0 w-56 z-50 mt-2 origin-top-right bg-white rounded shadow-lg">
+              <div className="p-1">
                 <Menu.Item>
                   <Button
                     className="flex w-full hover:bg-orange-500 hover:text-white text-gray-700 rounded p-2 normal-case font-normal group transition-colors"
